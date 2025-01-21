e3f7d02a71b8f56a199555d1a3f5578cd03531e2
LitteBoyS2CMD <mungnvph52815@gmail.com>
2025-01-21 21:06:46 +0700

    video16

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ const app = express();
 const port = 3000;
 
 app.use(express.static(path.join(__dirname,'public')));
+app.use(express.urlencoded({
+    extended: true
+}
+)); // Xử lý dữ liệu từ form khi submit lên
+app.use(express.json()); 
 app.engine('hbs', handlebars.engine({
     extname: '.hbs'
 }));
@@ -13,5 +18,18 @@ app.set('view engine','hbs')
 app.set('views', path.join(__dirname, 'resources\\views'));
 
 app.use(morgan('combined'));
-app.get('/', (req,res) => res.render('home'));
+app.get('/', (req,res) => {
+    res.render('home')
+});
+app.get('/news', (req,res) => { 
+    res.render('news')
+});
+app.get('/search', (req,res) => { 
+    res.render('search')
+});
+app.post('/search', (req,res) => {
+    console.log(req.body);
+    res.send('')
+});
+
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
